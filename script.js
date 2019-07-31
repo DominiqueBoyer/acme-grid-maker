79ed3523e1b62116a227fce6de9c75fcebf9a633
Dominique <dominiqueboyer94@yahoo.com>
2019-07-30 22:03:07 -0700

    finish project

---
+++ script.js
@@ -0,0 +1,15 @@
+const button = document.querySelector('button');
+const inputRow = document.querySelector('input [name=rows]');
+const form =document.querySelector('form');
+
+
+form
+
+button.addEventListener('submit',(ev)=>
+{
+  console.log('hi');
+  ev.preventDefault();
+  console.log('hi');
+});
+
+
